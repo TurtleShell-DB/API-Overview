f2cb80fd5567ffabc2eb723291ff04d4207d608a
Joseph Lio <josephlio@hotmail.com>
2021-05-07 13:22:43 -0700

    Progress success set up readline implementation to search for end quotes

--- server/db/csv-parser/parse-photos.js
+++ server/db/csv-parser/parse-photos.js
@@ -1,22 +1,3 @@
-// const csv = require('csvtojson');
-// const db = require('../index.js');
-
-// const csvFilePath = './server/db/data/photos.csv';
-
-// csv()
-//   .fromFile(csvFilePath)
-//   .then((jsonObj) => {
-//     jsonObj.forEach((row) => {
-//       let { id, styleId, url, thumbnail_url } = row;
-//       db.queryAsync('INSERT INTO Photos SET photoID=?, styleID=?, url=?, thumbnailUrl=?', [id, styleId, url, thumbnail_url])
-//         // .then(() => console.log('successfully inserted into database!'))
-//         .catch((err) => console.log(err));
-//     });
-//   })
-//   .then(() => console.log('Import Complete!'));
-
-// Async / await usage
-// const jsonArray = await csv().fromFile(csvFilePath);
 const readline = require('readline');
 const fs = require('fs');
 const fastcsv = require('fast-csv');
@@ -42,16 +23,30 @@ db.connectAsync()
   .then(() => db.queryAsync(`USE ${database}`))
   .then(() => createTables(db))
   .then(() => {
+    // const stream = fs.createReadStream(csvFilePath);
     const rl = readline.createInterface({
-      input: fs.createReadStream(csvFilePath),
+      input: require('fs').createReadStream(csvFilePath),
     });
 
-    const stream = fs.createReadStream(csvFilePath);
+    rl.on('line', (line) => {
+      if (line[line.length - 1] !== '"') {
+        line += '"';
+      }
+      csvStream.write(line + '\n');
+    });
+
+    rl.on('close', () => {
+      csvStream.end();
+      // parseCSV();
+    });
+
+    // const parseCSV = () => {
     const csvData = [];
     const csvStream = fastcsv
       .parse()
       .on('data', (data) => {
-        const row = [];
+        console.log(data);
+        // const row = [];
         // row.push(parseInt(data[0]), parseInt(data[1]), data[2].toString, data[3].toString);
         if (data.length !== 4 || typeof data[3] !== 'string' || data[0] === 'id') {
           // console.log('error!!!!');
@@ -61,12 +56,37 @@ db.connectAsync()
         }
       })
       .on('end', () => {
+        console.log('csvdata is ', csvData);
         db.queryAsync('INSERT INTO Photos (photoID, styleID, url, thumbnailUrl) VALUES ?', [csvData])
           .then(() => console.log('Successfully imported!'))
           .catch((err) => console.log(err));
       });
-    stream.pipe(csvStream);
+      // stream.pipe(csvStream);
+    // };
+
+
   });
+
+// Async / await usage
+// const jsonArray = await csv().fromFile(csvFilePath);
+
+
+// const csv = require('csvtojson');
+// const db = require('../index.js');
+
+// const csvFilePath = './server/db/data/photos.csv';
+
+// csv()
+//   .fromFile(csvFilePath)
+//   .then((jsonObj) => {
+//     jsonObj.forEach((row) => {
+//       let { id, styleId, url, thumbnail_url } = row;
+//       db.queryAsync('INSERT INTO Photos SET photoID=?, styleID=?, url=?, thumbnailUrl=?', [id, styleId, url, thumbnail_url])
+//         // .then(() => console.log('successfully inserted into database!'))
+//         .catch((err) => console.log(err));
+//     });
+//   })
+//   .then(() => console.log('Import Complete!'));
 
 // Async / await usage
 // const jsonArray = await csv().fromFile(csvFilePath);
